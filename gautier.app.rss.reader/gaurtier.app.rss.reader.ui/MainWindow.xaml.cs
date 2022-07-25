ad796464891a5927c4d0243d1d45169bbd87d903
Michael Gautier <1744440+michaelgautier@users.noreply.github.com>
2022-07-24 20:55:33 -0500

    Generate initial screen structure using SQL Server database.

--- gautier.app.rss.reader/gaurtier.app.rss.reader.ui/MainWindow.xaml.cs
+++ gautier.app.rss.reader/gaurtier.app.rss.reader.ui/MainWindow.xaml.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
-namespace gaurtier.app.rss.reader.ui
+namespace gautier.app.rss.reader.ui
 {
     /// <summary>
     /// Root layout frame for RSS Reader
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Button _reader_button = null;
+        private TabControl _reader_tabs = null;
+        private List<TabItem> _reader_tab_items = new List<TabItem>();
+
+        private StackPanel _reader_feed_detail = new StackPanel
+        {
+            Orientation = Orientation.Vertical
+        };
+
+        private TextBlock _reader_feed_name = new TextBlock();
+        private TextBlock _reader_headline = new TextBlock();
+        private TextBlock _reader_article = new TextBlock();
+        private string _article_url = string.Empty;
 
         public MainWindow()
         {
@@ -20,14 +33,55 @@ namespace gaurtier.app.rss.reader.ui
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            _reader_button = new Button
+            _reader_tabs = new TabControl
             {
-                Content = "reader"
+                Background = Brushes.Orange,
+                BorderBrush = Brushes.Beige,
+                BorderThickness = new Thickness(1),
+                Padding = new Thickness(1)
             };
 
-            root_content.Children.Add(_reader_button);
+            _reader_tabs.SelectionChanged += _reader_tabs_SelectionChanged;
+
+            var feeds = feed_data_exchange.get_all_feeds();
+
+            var feed_names = feeds.Keys;
+
+            foreach (var feed_name in feed_names)
+            {
+                var reader_tab = new TabItem
+                {
+                    Header = feed_name
+                };
+
+                _reader_tab_items.Add(reader_tab);
+                _reader_tabs.Items.Add(reader_tab);
+            }
+
+            root_content.Children.Add(_reader_tabs);
+            root_content.Children.Add(_reader_feed_detail);
+
+            _reader_feed_detail.Children.Add(_reader_feed_name);
+            _reader_feed_detail.Children.Add(_reader_headline);
+            _reader_feed_detail.Children.Add(_reader_article);
 
             return;
         }
+
+        private void _reader_tabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var feed_index = _reader_tabs.SelectedIndex;
+
+            if(feed_index > -1 && feed_index < _reader_tab_items.Count)
+            {
+                var reader_tab_item = _reader_tab_items[feed_index];
+
+                _reader_feed_name.Text = $"{reader_tab_item.Header}";
+            }
+
+            return;
+        }
+
+
     }
 }
---
+++ gautier.app.rss.reader/gaurtier.app.rss.reader.ui/feed_article.cs
@@ -0,0 +1,18 @@
+﻿namespace gautier.app.rss.reader.ui
+{
+    internal class feed_article
+    {
+        internal string feed_name { get; set; } = string.Empty;
+        internal string headline_text { get; set; } = string.Empty;
+        internal string article_summary { get; set; } = string.Empty;
+        internal string article_text { get; set; } = string.Empty;
+        internal string article_date { get; set; } = string.Empty;
+        internal string article_url { get; set; } = string.Empty;
+        internal string row_insert_date_time { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{headline_text} {article_url} {article_date}";
+        }
+    }
+}
