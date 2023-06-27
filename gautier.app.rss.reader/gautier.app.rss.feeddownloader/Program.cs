247f679fb1679c3c625680c858b621ce2c25ba4a
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 <1744440+michaelgautier@users.noreply.github.com>
2023-06-27 08:16:37 -0500

    Added Microsoft Synchronization API to streamline integration with project API.

---
+++ gautier.app.rss.reader/gautier.app.rss.feeddownloader/Program.cs
@@ -0,0 +1,53 @@
+﻿using gautier.rss.data;
+using System.Xml;
+
+namespace gautier.app.rss.feeddownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string feedSaveDirectoryPath = @"C:\RSSFeeds\";
+
+            if (Directory.Exists(feedSaveDirectoryPath) == false)
+            {
+                Directory.CreateDirectory(feedSaveDirectoryPath);
+            }
+
+            List<Feed> FeedInfos = new List<Feed>
+            {
+                new Feed{
+                    FeedName = "Ars Technica",
+                    FeedUrl = "https://feeds.arstechnica.com/arstechnica/index"
+                },
+                new Feed{
+                    FeedName = "Slashdot",//no more than once per 30 minutes
+                    FeedUrl = "http://rss.slashdot.org/Slashdot/slashdotMainatom"
+                },
+                new Feed{
+                    FeedName = "Phoronix",
+                    FeedUrl = "https://www.phoronix.com/phoronix-rss.php"
+                },
+                new Feed{
+                    FeedName = "DistroWatch",
+                    FeedUrl = "https://distrowatch.com/news/dw.xml"
+                }
+            };
+
+            foreach (var FeedInfo in FeedInfos)
+            {
+                using (var feedXml = XmlReader.Create(FeedInfo.FeedUrl))
+                {
+                    var FeedFilePath = Path.Combine(feedSaveDirectoryPath, $"{FeedInfo.FeedName}.xml");
+
+                    using(var feedXmlWriter = XmlWriter.Create(FeedFilePath))
+                    {
+                        feedXmlWriter.WriteNode(feedXml, true);
+                    }
+                }
+            }
+
+            Console.WriteLine("Gautier RSS Reader WPF");
+        }
+    }
+}
---
+++ gautier.app.rss.reader/gautier.rss.data/RSSNetClient.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace gautier.rss.data
+{
+    /// <summary>
+    /// Handles network communication. Translates rss XML data into C# object format.
+    /// </summary>
+    public class RSSNetClient
+    {
+        public List<FeedArticle> GetArticles(Feed feedInfo)
+        {
+            List<FeedArticle> Articles = new();
+
+            SyndicationFeed ExternalFeed;
+
+            using(var FeedReader = XmlReader.Create(feedInfo.FeedUrl))
+            {
+                ExternalFeed = SyndicationFeed.Load(FeedReader);
+            }
+
+            return Articles;
+        }
+    }
+}
