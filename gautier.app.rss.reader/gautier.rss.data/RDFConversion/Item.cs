1918c30d26a8723304d1c1608da4d4035307246f
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 <1744440+michaelgautier@users.noreply.github.com>
2023-06-30 09:58:44 -0500

    Used ChatGPT to generate an RDF object instance to transform from RDF to RSS.

---
+++ gautier.app.rss.reader/gautier.rss.data/RDFConversion/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+namespace gautier.rss.data.RDFConversion
+{
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; } = string.Empty;
+
+        [XmlElement(ElementName = "link")]
+        public string Link { get; set; } = string.Empty;
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; } = string.Empty;
+
+        [XmlElement(ElementName = "dc:date", Namespace = "http://purl.org/dc/elements/1.1/")]
+        public string Date { get; set; } = string.Empty;
+
+        [XmlElement(ElementName = "slash:section", Namespace = "http://purl.org/rss/1.0/modules/slash/")]
+        public string Section { get; set; } = string.Empty;
+    }
+}
---
+++ gautier.app.rss.reader/gautier.rss.data/RDFConversion/RDFConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+
+namespace gautier.rss.data.RDFConversion
+{
+    public static class RDFConverter
+    {
+        public static RdfRDF CreateRDF(string rdfFilePath)
+        {
+            RdfRDF RDFDoc = new();
+
+            var serializer = new XmlSerializer(typeof(RdfRDF));
+
+            using (StringReader reader = new StringReader(File.ReadAllText(rdfFilePath)))
+            {
+                RdfRDF? rdfRdf = serializer.Deserialize(reader) as RdfRDF;
+
+                if (rdfRdf != null)
+                {
+                    RDFDoc = rdfRdf;
+                }
+            }
+
+            return RDFDoc;
+        }
+    }
+}
