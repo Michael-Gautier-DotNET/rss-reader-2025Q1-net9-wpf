f909cd5e7d8268269f7e91b261374d2510448884
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-10 15:28:16 -0500

    Relocated rdbms integration that adds or modifies RSS tables into their own class/modules.

---
+++ gautier.app.rss.reader/gautier.rss.data/RSSDb/FeedArticleWriter.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SQLite;
+using System.Text;
+
+namespace gautier.rss.data.RSSDb
+{
+    internal class FeedArticleWriter
+    {
+        private static readonly string[] _ColumnNames = FeedArticleReader.GetSQLFeedsArticlesColumnNames();
+
+        internal static void AddFeedArticle(SQLiteConnection sqlConn, FeedArticleUnion article)
+        {
+            StringBuilder CommandText = SQLUtil.CreateSQLInsertCMDText(FeedArticleReader.TableName, _ColumnNames);
+
+            using (SQLiteCommand SQLCmd = new(CommandText.ToString(), sqlConn))
+            {
+                FeedArticleReader.CreateFeedArticleParameters(SQLCmd, article.ArticleDetail, _ColumnNames);
+
+                SQLCmd.ExecuteNonQuery();
+            }
+
+            return;
+        }
+
+        internal static void ModifyFeedArticle(SQLiteConnection sqlConn, FeedArticleUnion article)
+        {
+            StringBuilder CommandText = SQLUtil.CreateSQLUpdateCMDText(FeedArticleReader.TableName, _ColumnNames);
+            CommandText.Append("feed_name = @feed_name AND article_url = @article_url;");
+
+            using (SQLiteCommand SQLCmd = new(CommandText.ToString(), sqlConn))
+            {
+                FeedArticleReader.CreateFeedArticleParameters(SQLCmd, article.ArticleDetail, _ColumnNames);
+
+                SQLCmd.ExecuteNonQuery();
+            }
+
+            return;
+        }
+    }
+}
---
+++ gautier.app.rss.reader/gautier.rss.data/RSSDb/FeedWriter.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SQLite;
+using System.Text;
+
+namespace gautier.rss.data.RSSDb
+{
+    internal class FeedWriter
+    {
+        private static readonly string[] _ColumnNames = FeedReader.GetSQLFeedsColumnNames();
+
+        internal static void AddFeed(SQLiteConnection sqlConn, Feed feedHeader)
+        {
+            StringBuilder CommandText = SQLUtil.CreateSQLInsertCMDText(FeedReader.TableName, _ColumnNames);
+
+            using (SQLiteCommand SQLCmd = new(CommandText.ToString(), sqlConn))
+            {
+                FeedReader.CreateFeedParameters(SQLCmd, feedHeader, _ColumnNames);
+
+                SQLCmd.ExecuteNonQuery();
+            }
+
+            return;
+        }
+
+        internal static void ModifyFeed(SQLiteConnection sqlConn, Feed feedHeader)
+        {
+            StringBuilder CommandText = SQLUtil.CreateSQLUpdateCMDText(FeedReader.TableName, _ColumnNames);
+            CommandText.Append("feed_name = @feed_name;");
+
+            using (SQLiteCommand SQLCmd = new(CommandText.ToString(), sqlConn))
+            {
+                FeedReader.CreateFeedParameters(SQLCmd, feedHeader, _ColumnNames);
+
+                SQLCmd.ExecuteNonQuery();
+            }
+
+            return;
+        }
+    }
+}
