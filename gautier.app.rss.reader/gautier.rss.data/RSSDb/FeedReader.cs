cce1f30d77ee3023a0bd004dbc15e4d849b99d68
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-10 14:18:59 -0500

    Consolidate attached rdbms data functions to simplify expression of dependencies.

---
+++ gautier.app.rss.reader/gautier.rss.data/RSSDb/FeedReader.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SQLite;
+
+namespace gautier.rss.data.RSSDb
+{
+    public class FeedReader
+    {
+        public static int GetFeedCount(SQLiteConnection sqlConn)
+        {
+            int FeedCount = 0;
+
+            string RowCheckCommandText = $"SELECT COUNT(*) FROM feed;";
+
+            using (SQLiteCommand RowCheckSQLCmd = new(RowCheckCommandText, sqlConn))
+            {
+                FeedCount = Convert.ToInt32(RowCheckSQLCmd.ExecuteScalar());
+            }
+
+            return FeedCount;
+        }
+
+        public static int GetFeedCount(SQLiteConnection sqlConn, string feedName)
+        {
+            int FeedCount = 0;
+
+            string RowCheckCommandText = $"SELECT COUNT(*) FROM feed WHERE feed_name = @FeedName;";
+
+            using (SQLiteCommand RowCheckSQLCmd = new(RowCheckCommandText, sqlConn))
+            {
+                RowCheckSQLCmd.Parameters.AddWithValue("@FeedName", feedName);
+
+                FeedCount = Convert.ToInt32(RowCheckSQLCmd.ExecuteScalar());
+            }
+
+            return FeedCount;
+        }
+    }
+}
