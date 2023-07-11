88e7e1244b71ec434cb39692e3be6edeb6be09bb
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-11 14:48:12 -0500

    Streamlined how the data access process is defined. Simplified the representation of this process to make it easier to tune and understand at a glance.

--- gautier.app.rss.reader/gautier.rss.data/FeedDataExchange.cs
+++ gautier.app.rss.reader/gautier.rss.data/FeedDataExchange.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using System.Data.SQLite;
+﻿using System.Data.SQLite;
 using System.Globalization;
 
 using gautier.rss.data.RSSDb;
@@ -14,13 +13,13 @@ public static class FeedDataExchange
 
     public static SortedList<string, Feed> GetAllFeeds(string sqlConnectionString)
     {
-        var Feeds = new SortedList<string, Feed>();
+        SortedList<string, Feed> Feeds = new();
 
         using (SQLiteConnection SQLConn = SQLUtil.OpenSQLiteConnection(sqlConnectionString))
         {
-            var FeedEntries = FeedReader.GetAllRows(SQLConn);
+            List<Feed> FeedEntries = FeedReader.GetAllRows(SQLConn);
 
-            foreach (var FeedEntry in FeedEntries)
+            foreach (Feed FeedEntry in FeedEntries)
             {
                 Feeds[FeedEntry.FeedName] = FeedEntry;
             }
@@ -31,174 +30,58 @@ public static class FeedDataExchange
 
     public static SortedList<string, SortedList<string, FeedArticle>> GetAllFeedArticles(string sqlConnectionString)
     {
-        var feeds_articles = new SortedList<string, SortedList<string, FeedArticle>>(100);
+        SortedList<string, SortedList<string, FeedArticle>> Articles = new(100);
 
-        using (var sql_conn = new SQLiteConnection(sqlConnectionString))
+        using (SQLiteConnection SQLConn = SQLUtil.OpenSQLiteConnection(sqlConnectionString))
         {
-            sql_conn.Open();
+            List<FeedArticle> FeedArticles = FeedArticleReader.GetAllRows(SQLConn);
 
-            using (var sql_com = sql_conn.CreateCommand())
+            foreach (FeedArticle ArticleEntry in FeedArticles)
             {
-                sql_com.CommandType = CommandType.StoredProcedure;
-                sql_com.CommandText = "get_all_feeds_articles";
+                string FeedName = ArticleEntry.FeedName;
 
-                using (var col_reader = sql_com.ExecuteReader())
+                if (Articles.ContainsKey(FeedName) == false)
                 {
-                    var col_count = col_reader.FieldCount;
+                    Articles[FeedName] = new(1000);
+                }
 
-                    while (col_reader.Read())
+                if (Articles.ContainsKey(FeedName))
+                {
+                    string ArticleUrl = ArticleEntry.ArticleUrl;
+
+                    SortedList<string, FeedArticle> ArticlesByUrl = Articles[FeedName];
+
+                    if (ArticlesByUrl.ContainsKey(ArticleUrl) == false)
                     {
-                        var FeedName = string.Empty;
-                        var HeadlineText = string.Empty;
-                        var ArticleSummary = string.Empty;
-                        var ArticleText = string.Empty;
-                        var ArticleDate = string.Empty;
-                        var ArticleUrl = string.Empty;
-                        var RowInsertDateTime = string.Empty;
-
-                        for (var col_index = 0; col_index < col_count; col_index++)
-                        {
-                            var col_value = $"{col_reader.GetValue(col_index)}";
-                            //Assume all is text/string.
-                            switch (col_index)
-                            {
-                                case 0: //feed_name
-                                    FeedName = col_value;
-                                    break;
-                                case 1://headline_text
-                                    HeadlineText = col_value;
-                                    break;
-                                case 2://article_summary
-                                    ArticleSummary = col_value;
-                                    break;
-                                case 3://article_text
-                                    ArticleText = col_value;
-                                    break;
-                                case 4://article_date
-                                    ArticleDate = col_value;
-                                    break;
-                                case 5://article_url
-                                    ArticleUrl = col_value;
-                                    break;
-                                case 6://row_insert_date_time
-                                    RowInsertDateTime = col_value;
-                                    break;
-                            }
-                        }
-
-                        if (!feeds_articles.ContainsKey(FeedName))
-                        {
-                            feeds_articles[FeedName] = new SortedList<string, FeedArticle>(1000);
-                        }
-
-                        if (feeds_articles.ContainsKey(FeedName))
-                        {
-                            var articles = feeds_articles[FeedName];
-
-                            if (!articles.ContainsKey(ArticleUrl))
-                            {
-                                FeedArticle feed_article_item = new FeedArticle
-                                {
-                                    FeedName = FeedName,
-                                    HeadlineText = HeadlineText,
-                                    ArticleSummary = ArticleSummary,
-                                    ArticleText = ArticleText,
-                                    ArticleDate = ArticleDate,
-                                    ArticleUrl = ArticleUrl,
-                                    RowInsertDateTime = RowInsertDateTime
-                                };
-
-                                articles.Add(ArticleUrl, feed_article_item);
-                            }
-                        }
+                        ArticlesByUrl.Add(ArticleUrl, ArticleEntry);
                     }
                 }
             }
         }
 
-        return feeds_articles;
+        return Articles;
     }
 
     public static SortedList<string, FeedArticle> GetFeedArticles(string sqlConnectionString, string feedName)
     {
-        var feeds_articles = new SortedList<string, FeedArticle>(100);
+        SortedList<string, FeedArticle> Articles = new(100);
 
-        using (var sql_conn = new SQLiteConnection(sqlConnectionString))
+        using (SQLiteConnection SQLConn = SQLUtil.OpenSQLiteConnection(sqlConnectionString))
         {
-            sql_conn.Open();
+            List<FeedArticle> FeedArticles = FeedArticleReader.GetRows(SQLConn, feedName);
 
-            using (var sql_com = sql_conn.CreateCommand())
+            foreach (FeedArticle ArticleEntry in FeedArticles)
             {
-                sql_com.CommandType = CommandType.StoredProcedure;
-                sql_com.CommandText = "get_feed_articles";
+                string ArticleUrl = ArticleEntry.ArticleUrl;
 
-                sql_com.Parameters.AddWithValue("@feed_name", feedName);
-
-                using (var col_reader = sql_com.ExecuteReader())
+                if (Articles.ContainsKey(ArticleUrl) == false)
                 {
-                    var col_count = col_reader.FieldCount;
-
-                    while (col_reader.Read())
-                    {
-                        var FeedName = string.Empty;
-                        var HeadlineText = string.Empty;
-                        var ArticleSummary = string.Empty;
-                        var ArticleText = string.Empty;
-                        var ArticleDate = string.Empty;
-                        var ArticleUrl = string.Empty;
-                        var RowInsertDateTime = string.Empty;
-
-                        for (var col_index = 0; col_index < col_count; col_index++)
-                        {
-                            var col_value = $"{col_reader.GetValue(col_index)}";
-                            //Assume all is text/string.
-                            switch (col_index)
-                            {
-                                case 0: //feed_name
-                                    FeedName = col_value;
-                                    break;
-                                case 1://headline_text
-                                    HeadlineText = col_value;
-                                    break;
-                                case 2://article_summary
-                                    ArticleSummary = col_value;
-                                    break;
-                                case 3://article_text
-                                    ArticleText = col_value;
-                                    break;
-                                case 4://article_date
-                                    ArticleDate = col_value;
-                                    break;
-                                case 5://article_url
-                                    ArticleUrl = col_value;
-                                    break;
-                                case 6://row_insert_date_time
-                                    RowInsertDateTime = col_value;
-                                    break;
-                            }
-                        }
-
-                        if (!feeds_articles.ContainsKey(ArticleUrl))
-                        {
-                            FeedArticle feed_article_item = new FeedArticle
-                            {
-                                FeedName = FeedName,
-                                HeadlineText = HeadlineText,
-                                ArticleSummary = ArticleSummary,
-                                ArticleText = ArticleText,
-                                ArticleDate = ArticleDate,
-                                ArticleUrl = ArticleUrl,
-                                RowInsertDateTime = RowInsertDateTime
-                            };
-
-                            feeds_articles.Add(ArticleUrl, feed_article_item);
-                        }
-                    }
+                    Articles.Add(ArticleUrl, ArticleEntry);
                 }
             }
         }
 
-        return feeds_articles;
+        return Articles;
     }
 
     public static void ImportStaticFeedFilesToDatabase(string feedSaveDirectoryPath, string feedDbFilePath, Feed[] feedInfos)
