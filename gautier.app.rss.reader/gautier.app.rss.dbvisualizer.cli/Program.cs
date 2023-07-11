6f4e19fefd4be8ef3a9d3e1a3ab8e9150ba917e6
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-11 12:40:38 -0500

    RSS Db Visualizer is a check on the Feed Download process to validate the process prior to integration with a UI. At the same time, this will evolve the project towards the proper API representation for the UI in terms of visualizing the RSS feed data.

---
+++ gautier.app.rss.reader/gautier.app.rss.dbvisualizer.cli/Program.cs
@@ -0,0 +1,30 @@
+﻿using gautier.rss.data;
+using gautier.rss.data.RSSDb;
+
+namespace gautier.app.rss.dbvisualizer.cli
+{
+    internal static class Program
+    {
+        private static readonly string _FeedDbFilePath = @"C:\RSSFeeds\feeds_db\rss.db";
+
+        internal static void Main(string[] args)
+        {
+            string SQLiteDbConnectionString = SQLUtil.GetSQLiteConnectionString(_FeedDbFilePath, 3);
+
+            SortedList<string, Feed> RSSFeeds = FeedDataExchange.GetAllFeeds(SQLiteDbConnectionString);
+
+            List<string> RSSFeedNames = new(RSSFeeds.Keys);
+
+            foreach(string FeedName in RSSFeedNames)
+            {
+                Console.WriteLine($"Feed:\t{FeedName}");
+
+                /*
+                 * Output article information.
+                 */
+            }
+
+            return;
+        }
+    }
+}
