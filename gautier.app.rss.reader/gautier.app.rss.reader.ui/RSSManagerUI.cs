79186928d71268515d8f79c73bc3abebea81c7ae
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-14 18:15:36 -0500

    Define text input to modify feed name, url, retrieve limits, and retention timeframe.

--- gautier.app.rss.reader/gautier.app.rss.reader.ui/RSSManagerUI.cs
+++ gautier.app.rss.reader/gautier.app.rss.reader.ui/RSSManagerUI.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace gautier.app.rss.reader.ui
 {
     public partial class RSSManagerUI : Window
     {
+        private readonly Label _FeedNameLabel = new()
+        {
+            Content = "Feed Name"
+        };
+
+        private readonly Label _FeedUrlLabel = new()
+        {
+            Content = "Feed Url"
+        };
+
+        private readonly Label _LastRetrievedLabel = new()
+        {
+            Content = "Last Retrieved"
+        };
+
+        private readonly Label _RetrieveLimitHrsLabel = new()
+        {
+            Content = "Retrieve Limit (in Hours)"
+        };
+
+        private readonly Label _RetentionDaysLabel = new()
+        {
+            Content = "Auto Delete Articles after x Days"
+        };
+
+        private readonly TextBox _FeedName = new()
+        {
+        };
+
+        private readonly TextBox _FeedUrl = new()
+        {
+        };
+
+        private readonly TextBox _LastRetrieved = new()
+        {
+        };
+
+        private readonly TextBox _RetrieveLimitHrs = new()
+        {
+        };
+
+        private readonly TextBox _RetentionDays = new()
+        {
+        };
+
+        private readonly Grid _UIRoot = new()
+        {
+            ShowGridLines = true,
+            VerticalAlignment = VerticalAlignment.Top
+        };
+
         public RSSManagerUI()
         {
             Initialized += RSSManagerUI_Initialized;
@@ -19,6 +71,41 @@ namespace gautier.app.rss.reader.ui
             Width = 800;
             SizeToContent = SizeToContent.Manual;
             WindowState = WindowState.Maximized;
+
+            UIElement[] Els =
+            {
+                _FeedNameLabel,
+                _FeedName,
+                _FeedUrlLabel,
+                _FeedUrl,
+                _LastRetrievedLabel,
+                _LastRetrieved,
+                _RetrieveLimitHrsLabel,
+                _RetrieveLimitHrs,
+                _RetentionDaysLabel,
+                _RetentionDays,
+            };
+
+            foreach (UIElement El in Els)
+            {
+                _UIRoot.Children.Add(El);
+            }
+
+            int HorizontalChildrenCount = _UIRoot.Children.Count;
+
+            for (int ColumnIndex = 0; ColumnIndex < HorizontalChildrenCount; ColumnIndex++)
+            {
+                ColumnDefinition ColDef = new()
+                {
+                    Width = new(1, GridUnitType.Star)
+                };
+
+                _UIRoot.ColumnDefinitions.Add(ColDef);
+
+                Grid.SetColumn(_UIRoot.Children[ColumnIndex], ColumnIndex);
+            }
+
+            Content = _UIRoot;
 
             return;
         }
