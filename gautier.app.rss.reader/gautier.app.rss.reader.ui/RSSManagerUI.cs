13fb35ca353026c306f82da1acc65d8163f60e0e
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-14 17:50:52 -0500

    Defining a UI for managing RSS feeds.

---
+++ gautier.app.rss.reader/gautier.app.rss.reader.ui/RSSManagerUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace gautier.app.rss.reader.ui
+{
+    public partial class RSSManagerUI : Window
+    {
+        public RSSManagerUI()
+        {
+            Initialized += RSSManagerUI_Initialized;
+
+            return;
+        }
+
+        private void RSSManagerUI_Initialized(object? sender, EventArgs e)
+        {
+            Title = "Gautier RSS | Feed Manager";
+            Height = 450;
+            Width = 800;
+            SizeToContent = SizeToContent.Manual;
+            WindowState = WindowState.Maximized;
+
+            return;
+        }
+    }
+}
