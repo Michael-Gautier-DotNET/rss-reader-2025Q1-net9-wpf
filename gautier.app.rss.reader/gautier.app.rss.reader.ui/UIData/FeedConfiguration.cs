2dcac34920381056e53b16982241541b85e2f6e7
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-15 14:32:04 -0500

    Centralized connection string and location of the attached SQLite database for all UI modules in the project/executable.

---
+++ gautier.app.rss.reader/gautier.app.rss.reader.ui/UIData/FeedConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+using gautier.rss.data.RSSDb;
+
+namespace gautier.app.rss.reader.ui.UIData
+{
+    internal class FeedConfiguration
+    {
+        internal static string FeedSaveDirectoryPath => Directory.GetCurrentDirectory();
+
+        internal static string FeedDbFilePath => @"rss.db";
+        internal static string SQLiteDbConnectionString => SQLUtil.GetSQLiteConnectionString(FeedDbFilePath, 3);
+    }
+}
