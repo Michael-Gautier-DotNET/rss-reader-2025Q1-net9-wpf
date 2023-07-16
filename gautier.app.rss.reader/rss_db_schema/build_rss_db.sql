22af8f1d6e8e5fab473b8c4fd1607d589b58b25c
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-16 01:36:27 -0500

    Id and Row Timestamps. Added a traditional numeric row key to the feeds table. Streamlines records modification through a user interface.

--- gautier.app.rss.reader/rss_db_schema/build_rss_db.sql
+++ gautier.app.rss.reader/rss_db_schema/build_rss_db.sql
@@ -1,19 +1,22 @@
 CREATE TABLE IF NOT EXISTS feeds_articles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
 	feed_name TEXT NOT NULL,
 	headline_text TEXT NOT NULL,
 	article_summary TEXT,
 	article_text TEXT,
 	article_date TEXT,
 	article_url TEXT NOT NULL,
-	row_insert_date_time TEXT NOT NULL DEFAULT 'datetime(''now'')'
+	row_insert_date_time TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS feeds (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
 	feed_name TEXT,
 	feed_url TEXT,
 	last_retrieved TEXT,
 	retrieve_limit_hrs TEXT,
-	retention_days TEXT
+	retention_days TEXT,
+	row_insert_date_time TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX IF NOT EXISTS index_feeds_articles_feed_name ON feeds_articles (
