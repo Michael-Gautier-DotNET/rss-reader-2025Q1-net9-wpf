1a061009133c53f937e866f53dbb858d388038b2
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 <1744440+michaelgautier@users.noreply.github.com>
2023-07-18 00:43:01 -0500

    Conversion functions simplify upstream callers that need to alternate between Feed object layouts.
    Several layouts for Feed exist application-wide. Referring to the Feed header. You have:
    
    - Xml Feed: Represents and parses data downloaded from websites. Foundation data representation.
    - Feed: A linear, flat representation of the feed. Maps to flat-file, tab-delimited formats. Format for SQLite database.
    - UI Feed: Version of a feed in Microsoft WPF format. Sets up a Feed header that takes advantage WPF visual update infrastructure.
    
    The UI Feed object is designed to be incorporated into a ObservableCollection<T> instance. That allows both the individual item and the list to work in the best way with Microsoft WPF.

--- gautier.app.rss.reader/gautier.app.rss.reader.ui/UIData/BindableFeed.cs
+++ gautier.app.rss.reader/gautier.app.rss.reader.ui/UIData/BindableFeed.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
+using System.Globalization;
 using System.Windows;
 
 using gautier.rss.data;
 
 namespace gautier.app.rss.reader.ui.UIData
 {
-    public class BindableFeed : DependencyObject, INotifyPropertyChanged
+    public class BindableFeed : DependencyObject
     {
+        private static readonly DateTimeFormatInfo _InvariantFormat = DateTimeFormatInfo.InvariantInfo;
+
         public static readonly DependencyProperty IdProperty = DependencyProperty.Register("Id", typeof(int), typeof(BindableFeed), new PropertyMetadata(-1));
 
         public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(BindableFeed), new PropertyMetadata(string.Empty));
 
-        public static readonly DependencyProperty UrlProperty = DependencyProperty.Register("Url", typeof(string), typeof(BindableFeed), new PropertyMetadata(string.Empty, UrlPropertyChanged));
+        public static readonly DependencyProperty UrlProperty = DependencyProperty.Register("Url", typeof(string), typeof(BindableFeed), new PropertyMetadata(string.Empty));
 
         public static readonly DependencyProperty LastRetrievedProperty = DependencyProperty.Register("LastRetrieved", typeof(DateTime), typeof(BindableFeed), new PropertyMetadata(DateTime.Now));
 
         public static readonly DependencyProperty RetrieveLimitHrsProperty = DependencyProperty.Register("RetrieveLimitHrs", typeof(int), typeof(BindableFeed), new PropertyMetadata(1));
 
         public static readonly DependencyProperty RetentionDaysProperty = DependencyProperty.Register("RetentionDays", typeof(int), typeof(BindableFeed), new PropertyMetadata(45));
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public event EventHandler UrlValidationFailed;
 
         public int Id
         {
@@ -66,62 +65,13 @@ namespace gautier.app.rss.reader.ui.UIData
             set => SetValue(RetentionDaysProperty, value);
         }
 
-        private static void UrlPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            BindableFeed BFeed = d as BindableFeed;
-
-            string PreviousUrl = e.OldValue as string;
-            string NewUrl = e.NewValue as string;
-
-            bool IsValidUrl = RSSNetClient.ValidateUrlIsHttpOrHttps(NewUrl);
-
-            if (IsValidUrl)
-            {
-                BFeed.Url = NewUrl;
-                BFeed.OriginalUrl = PreviousUrl;
-
-                BFeed.OnPropertyChanged(nameof(Url));
-            }
-            else
-            {
-                BFeed.Url = PreviousUrl;
-                BFeed.OnUrlValidationFailed(PreviousUrl);
-            }
-
-            return;
-        }
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-            return;
-        }
-
-        protected virtual void OnUrlValidationFailed(string previousUrl)
-        {
-            UrlValidationFailed?.Invoke(this, EventArgs.Empty);
-
-            return;
-        }
-
         internal static ObservableCollection<BindableFeed> ConvertFeeds(SortedList<string, Feed> feeds)
         {
             ObservableCollection<BindableFeed> BFeeds = new();
 
             foreach (Feed FeedEntry in feeds.Values)
             {
-                BindableFeed BFeed = new()
-                {
-                    Id = FeedEntry.DbId,
-                    Name = FeedEntry.FeedName,
-                    Url = FeedEntry.FeedUrl,
-                    OriginalName = FeedEntry.FeedName,
-                    OriginalUrl = FeedEntry.FeedUrl,
-                    LastRetrieved = DateTime.Parse(FeedEntry.LastRetrieved),
-                    RetrieveLimitHrs = int.Parse(FeedEntry.RetrieveLimitHrs),
-                    RetentionDays = int.Parse(FeedEntry.RetentionDays),
-                };
+                BindableFeed BFeed = ConvertFeed(FeedEntry);
 
                 BFeeds.Add(BFeed);
             }
@@ -150,9 +100,43 @@ namespace gautier.app.rss.reader.ui.UIData
                 DbId = feed.Id,
                 FeedName = feed.Name,
                 FeedUrl = feed.Url,
-                LastRetrieved = $"{feed.LastRetrieved}",
+                LastRetrieved = feed.LastRetrieved.ToString(_InvariantFormat.UniversalSortableDateTimePattern),
                 RetrieveLimitHrs = $"{feed.RetrieveLimitHrs}",
                 RetentionDays = $"{feed.RetentionDays}",
+            };
+        }
+
+        internal static BindableFeed ConvertFeed(Feed feed)
+        {
+            return new()
+            {
+                Id = feed.DbId,
+                Name = feed.FeedName,
+                Url = feed.FeedUrl,
+                OriginalName = feed.FeedName,
+                OriginalUrl = feed.FeedUrl,
+                LastRetrieved = DateTime.Parse(feed.LastRetrieved),
+                RetrieveLimitHrs = int.Parse(feed.RetrieveLimitHrs),
+                RetentionDays = int.Parse(feed.RetentionDays),
+            };
+        }
+
+        internal static BindableFeed ConvertFeedNarrow(Feed feed)
+        {
+            return new()
+            {
+                /*
+                 * Do not bring over these fields:
+                    OriginalName = feed.FeedName,
+                    OriginalUrl = feed.FeedUrl,
+                 * They are used as change detection values and must remain constant.
+                 */
+                Id = feed.DbId,
+                Name = feed.FeedName,
+                Url = feed.FeedUrl,
+                LastRetrieved = DateTime.Parse(feed.LastRetrieved),
+                RetrieveLimitHrs = int.Parse(feed.RetrieveLimitHrs),
+                RetentionDays = int.Parse(feed.RetentionDays),
             };
         }
     }
